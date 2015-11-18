38abdaca3d45dd95bdc74b7a8e4c3d26be690660
kjpgit <kjpgit@users.noreply.github.com>
2015-11-18 08:47:12 -0600

    math cleanup

--- fire/Math.swift
+++ fire/Math.swift
@@ -4,17 +4,25 @@ private let PI = 3.1415926535
 private let RANDOM_RANGE_DEBUG = false
 
 
-// Return number of microseconds
-func get_current_timestamp() -> Int64 {
-    var time:timeval = timeval(tv_sec: 0, tv_usec: 0)
-    gettimeofday(&time, nil)
-    let curtime = Int64(time.tv_sec) * 1000000 + Int64(time.tv_usec)
-    return curtime
+struct Vector3 {
+    var x: Float
+    var y: Float
+    var z: Float
+
+    func length() -> Float {
+        return sqrt((x * x) + (y * y) + (z * z)) 
+    }
+}
+
+struct Color4 {
+    var r: Float
+    var g: Float
+    var b: Float
+    var a: Float
 }
 
 
-
-// Return number in range [0, 1]
+// Return Float in range [0, 1]
 func random_float() -> Float {
     let ret = Float(random()) / Float(Int32.max)
     precondition(ret >= 0)
@@ -23,7 +31,7 @@ func random_float() -> Float {
 }
 
 
-// Return number in range [lower, upper]
+// Return Float in range [lower, upper]
 func random_range(lower: Float, _ upper: Float) -> Float {
     precondition(lower <= upper)
     if RANDOM_RANGE_DEBUG { return random_choose(lower, upper); }
@@ -36,7 +44,7 @@ func random_range(lower: Float, _ upper: Float) -> Float {
 }
 
 
-// Return number in range [lower, upper]
+// Return Int in range [lower, upper]
 func random_range(lower: Int, _ upper: Int) -> Int {
     precondition(lower <= upper)
     if RANDOM_RANGE_DEBUG { return random_choose(lower, upper); }
@@ -59,37 +67,14 @@ func random_choose<T>(a: T, _ b: T) -> T {
 }
 
 
-struct Vector3 {
-    var x: Float
-    var y: Float
-    var z: Float
-
-    func length() -> Float {
-        return sqrt((x * x) + (y * y) + (z * z)) 
-    }
-}
-
-struct Color4 {
-    var r: Float
-    var g: Float
-    var b: Float
-    var a: Float
-}
-
-struct Float4 {
-    init (_ a: Float, _ b: Float, _ c: Float, _ d: Float) { 
-        self.a = a
-        self.b = b
-        self.c = c
-        self.d = d
-    }
-    var a: Float
-    var b: Float
-    var c: Float
-    var d: Float
-}
-
-// The length of these vectors are 1
+/*
+Return random 3D vector.  Length will be == 1.
+Source: gamedev.net
+This finds a random point on a circle, than finds the height of the sphere
+at that point.  It can use the top or bottom hemisphere for z.  
+This gives better distribution than two random angles (which will produce
+more points clustered at the poles)
+*/
 func RandomUniformUnitVector() -> Vector3 {
     let angle = random_range(0.0, Float(2.0 * PI))
     let r = sqrt(random_range(0.0, 1.0))
@@ -103,11 +88,3 @@ func RandomUniformUnitVector2D() -> Vector3 {
     let angle = random_range(0.0, Float(2.0 * PI))
     return Vector3(x: cos(angle), y: sin(angle), z: 0)
 }
-
-
-/*
-//print(random(0.4))
-let v = RandomUniformUnitVector2D()
-print(v)
-print(v.length())
-*/
